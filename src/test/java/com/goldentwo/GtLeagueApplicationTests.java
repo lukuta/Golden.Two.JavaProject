d6ad5a4dd35645d7f77332640fde9be4d1843e15
kklimczak <konrad.klimczak.4@gmail.com>
2017-03-29 17:49:36 +0200

    init commit of spring application

---
+++ src/test/java/com/goldentwo/GtLeagueApplicationTests.java
@@ -0,0 +1,16 @@
+package com.goldentwo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GtLeagueApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
