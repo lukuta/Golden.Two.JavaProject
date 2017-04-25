3fc174922e44da3ac5aae0679aeab3ece7037780
lukuta <kutalukasz@gmail.com>
2017-04-25 22:18:37 +0200

    moved global api path to application.properties

--- src/main/java/com/goldentwo/controller/MatchRestEndpoint.java
+++ src/main/java/com/goldentwo/controller/MatchRestEndpoint.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "api/v1/match")
+@RequestMapping(value = "matches")
 public class MatchRestEndpoint {
 
     private MatchService matchService;
