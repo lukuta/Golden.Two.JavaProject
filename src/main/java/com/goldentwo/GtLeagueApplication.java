81d7f0b88d4c970b252b7fb913b4930ecbd1e777
lukuta <kutalukasz@gmail.com>
2017-04-27 10:37:40 +0200

    changed MBean registration policy to IGNORE_EXISTING

--- src/main/java/com/goldentwo/GtLeagueApplication.java
+++ src/main/java/com/goldentwo/GtLeagueApplication.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.EnableMBeanExport;
+import org.springframework.jmx.support.RegistrationPolicy;
 
 @SpringBootApplication
 @EnableCaching
-@EnableMBeanExport
+@EnableMBeanExport(registration = RegistrationPolicy.IGNORE_EXISTING)
 public class GtLeagueApplication {
 
 	public static void main(String[] args) {
