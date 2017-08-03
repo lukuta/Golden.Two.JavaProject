adeb977341308cf5152f3fcbd82c2d9eccd2a5ab
lkuta <lukasz.kuta@comarch.pl>
2017-08-03 11:55:27 +0200

    added team endpoint

---
+++ src/main/java/com/goldentwo/controller/TeamRestEndpoint.java
@@ -0,0 +1,58 @@
+package com.goldentwo.controller;
+
+import com.goldentwo.dto.TeamDto;
+import com.goldentwo.exception.TeamException;
+import com.goldentwo.service.TeamService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/teams")
+public class TeamRestEndpoint {
+    private final TeamService teamService;
+
+    @Autowired
+    public TeamRestEndpoint(TeamService teamService) {
+        this.teamService = teamService;
+    }
+
+    @GetMapping
+    public List<TeamDto> getAllTeams() {
+        return teamService.findAllTeams();
+    }
+
+    @GetMapping("{id}")
+    public TeamDto findTeamById(@PathVariable Long id) {
+        return teamService.findTeamById(id);
+    }
+
+    @GetMapping("/find")
+    public TeamDto findTeamByName(@RequestParam String name) {
+        return teamService.findTeamByName(name);
+    }
+
+    @PostMapping
+    public TeamDto createTeam(@RequestBody TeamDto teamDto) {
+        return teamService.saveTeam(teamDto);
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity deleteTeam(@PathVariable Long id) {
+        return teamService.deleteTeam(id);
+    }
+
+    @ExceptionHandler(TeamException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public Map<String, String> handleException(TeamException ex) {
+        Map<String, String> response = new HashMap<>();
+        response.put("Message", ex.getMessage());
+
+        return response;
+    }
+}
