6b5774ac4e498d7b62ee5ba28a93efabd822e27f
kklimczak <konrad.klimczak.4@gmail.com>
2017-08-08 22:42:59 +0200

    add Tournament entity and dto classes

---
+++ src/main/java/com/goldentwo/dto/TournamentDto.java
@@ -0,0 +1,19 @@
+package com.goldentwo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TournamentDto {
+
+    private Long id;
+    private String name;
+    private Set<TeamDto> teams;
+}
---
+++ src/main/java/com/goldentwo/model/Tournament.java
@@ -0,0 +1,42 @@
+package com.goldentwo.model;
+
+import com.goldentwo.dto.TeamDto;
+import com.goldentwo.dto.TournamentDto;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.NotEmpty;
+
+import javax.persistence.*;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Tournament {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String name;
+
+    @NotEmpty
+    @OneToMany
+    private Set<Team> teams;
+
+    public TournamentDto asDto() {
+        Set<TeamDto> teamDtos = teams.stream()
+                .map(Team::asDto)
+                .collect(Collectors.toSet());
+
+        return TournamentDto.builder()
+                .id(id).name(name).teams(teamDtos).build();
+    }
+
+}
