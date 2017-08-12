a5d3b1f88c7dea2ae3c8bb80412b7886f72f42d3
kklimczak <konrad.klimczak.4@gmail.com>
2017-08-12 22:04:52 +0200

    fix ambiguous requests in tournament rest endpoint

--- src/main/java/com/goldentwo/controller/TournamentRestEndpoint.java
+++ src/main/java/com/goldentwo/controller/TournamentRestEndpoint.java
@@ -1,13 +1,17 @@
 package com.goldentwo.controller;
 
 import com.goldentwo.dto.TournamentDto;
+import com.goldentwo.exception.TournamentException;
 import com.goldentwo.model.Tournament;
 import com.goldentwo.service.TournamentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/tournaments")
@@ -30,8 +34,8 @@ public class TournamentRestEndpoint {
         return tournamentService.findTournamentById(id);
     }
 
-    @GetMapping(value = "/{name}")
-    public TournamentDto findTournamentByName(@PathVariable String name) {
+    @GetMapping(value = "/find")
+    public TournamentDto findTournamentByName(@RequestParam String name) {
         return tournamentService.findTournamentByName(name);
     }
 
@@ -45,4 +49,13 @@ public class TournamentRestEndpoint {
         return tournamentService.deleteTournament(id);
     }
 
+    @ExceptionHandler(TournamentException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public Map<String, String> handleException(TournamentException ex) {
+        Map<String, String> response = new HashMap<>();
+        response.put("message", ex.getMessage());
+        response.put("code", HttpStatus.NOT_FOUND.toString());
+
+        return response;
+    }
 }
