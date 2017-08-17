29a6f5b378009d8e1660d58c0b9e60deb597b93d
lukuta <kutalukasz@gmail.com>
2017-08-17 22:53:43 +0200

    added button for logging in by facebook account

--- src/main/java/com/goldentwo/config/WebConfig.java
+++ src/main/java/com/goldentwo/config/WebConfig.java
@@ -30,7 +30,6 @@ public class WebConfig extends WebMvcConfigurerAdapter {
         registry.addViewController("/")
                 .setViewName("forward:/index");
         registry.addViewController("/index");
-        registry.addViewController("/securedPage");
     }
 
     @Override
