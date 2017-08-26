0bd6d71892fbf575dfb21b7f8661be467f108a7d
kklimczak <konrad.klimczak.4@gmail.com>
2017-08-26 11:19:12 +0200

    add league and round repositories

---
+++ src/main/java/com/goldentwo/repository/LeagueRepository.java
@@ -0,0 +1,12 @@
+package com.goldentwo.repository;
+
+import com.goldentwo.model.League;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface LeagueRepository extends JpaRepository<League, Long> {
+    public Optional<League> findByName(String name);
+}
