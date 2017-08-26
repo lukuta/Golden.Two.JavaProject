cf92185f45eb1fa8c37fbbcac63a4f406e07cd4a
kklimczak <konrad.klimczak.4@gmail.com>
2017-08-26 18:59:45 +0200

    add tests for find methods

---
+++ src/test/java/com/goldentwo/service/league/LeagueServiceTest.java
@@ -0,0 +1,90 @@
+package com.goldentwo.service.league;
+
+import com.goldentwo.dto.LeagueDto;
+import com.goldentwo.model.League;
+import com.goldentwo.repository.LeagueRepository;
+import com.goldentwo.service.impl.LeagueServiceImpl;
+import com.google.common.collect.Sets;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.Arrays;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Matchers.any;
+
+@ActiveProfiles("test")
+public class LeagueServiceTest {
+
+    @Mock
+    private LeagueRepository leagueRepository;
+
+    @InjectMocks
+    private LeagueServiceImpl sut;
+
+    private League leagueOne;
+    private League leagueTwo;
+
+    private LeagueDto leagueOneDto;
+    private LeagueDto leagueTwoDto;
+
+    @Before
+    public void initialize() {
+        MockitoAnnotations.initMocks(this);
+
+        leagueOne = League.builder().id(1L)
+                .name("ESL")
+                .actualRound(1)
+                .rounds(Sets.newHashSet())
+                .teams(Sets.newHashSet())
+                .build();
+
+        leagueOneDto = leagueOne.asDto();
+
+        leagueTwo = League.builder().id(2L)
+                .name("FaceIt")
+                .actualRound(1)
+                .rounds(Sets.newHashSet())
+                .teams(Sets.newHashSet())
+                .build();
+
+        leagueTwoDto = leagueTwo.asDto();
+    }
+
+    @Test
+    public void findAllLeaguesTest() {
+
+        Mockito
+                .when(leagueRepository.findAll())
+                .thenReturn(Arrays.asList(leagueOne, leagueTwo));
+
+        Set<LeagueDto> listFromSut = sut.findAllLeagues();
+
+        assertThat(listFromSut)
+                .isNotNull()
+                .isEqualTo(Sets.newHashSet(leagueOneDto, leagueTwoDto));
+
+    }
+
+    @Test
+    public void findLeagueByIdWhenExist() {
+        Long leagueId = 1L;
+
+        Mockito
+                .when(leagueRepository.findOne(leagueId))
+                .thenReturn(leagueOne);
+
+        LeagueDto leagueFromSut = sut.findLeagueById(leagueId);
+
+        assertThat(leagueFromSut)
+                .isNotNull()
+                .isEqualTo(leagueOneDto);
+    }
+}
