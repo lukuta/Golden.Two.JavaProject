5ada2a501dfb8eb276921fad04b4b57b8054793b
kklimczak <konrad.klimczak.4@gmail.com>
2017-08-26 11:30:48 +0200

    implement findAllLeagues and findLeagueById methods

--- src/main/java/com/goldentwo/service/LeagueService.java
+++ src/main/java/com/goldentwo/service/LeagueService.java
@@ -1,20 +1,19 @@
 package com.goldentwo.service;
 
 import com.goldentwo.dto.LeagueDto;
-import com.goldentwo.model.League;
 import org.springframework.http.ResponseEntity;
 
 import java.util.Set;
 
 public interface LeagueService {
 
-   Set<League> findAllLeagues();
+   Set<LeagueDto> findAllLeagues();
 
-   League findLeagueById(Long id);
+   LeagueDto findLeagueById(Long id);
 
-   League findLeagueByName(String name);
+   LeagueDto findLeagueByName(String name);
 
-   League saveLeague(LeagueDto leagueDto);
+   LeagueDto saveLeague(LeagueDto leagueDto);
 
    ResponseEntity deleteLeague(Long id);
 
--- src/main/java/com/goldentwo/service/impl/LeagueServiceImpl.java
+++ src/main/java/com/goldentwo/service/impl/LeagueServiceImpl.java
@@ -2,32 +2,40 @@ package com.goldentwo.service.impl;
 
 import com.goldentwo.dto.LeagueDto;
 import com.goldentwo.model.League;
+import com.goldentwo.repository.LeagueRepository;
 import com.goldentwo.service.LeagueService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 public class LeagueServiceImpl implements LeagueService {
-    
+
+    private LeagueRepository leagueRepository;
+
+    LeagueServiceImpl(LeagueRepository leagueRepository) {
+        this.leagueRepository = leagueRepository;
+    }
+
     @Override
-    public Set<League> findAllLeagues() {
+    public Set<LeagueDto> findAllLeagues() {
+        return leagueRepository.findAll().stream().map(League::asDto).collect(Collectors.toSet());
+    }
+
+    @Override
+    public LeagueDto findLeagueById(Long id) {
+        return leagueRepository.findOne(id).asDto();
+    }
+
+    @Override
+    public LeagueDto findLeagueByName(String name) {
         return null;
     }
 
     @Override
-    public League findLeagueById(Long id) {
-        return null;
-    }
-
-    @Override
-    public League findLeagueByName(String name) {
-        return null;
-    }
-
-    @Override
-    public League saveLeague(LeagueDto leagueDto) {
+    public LeagueDto saveLeague(LeagueDto leagueDto) {
         return null;
     }
 
